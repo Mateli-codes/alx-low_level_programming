5545baaf2a32f0af9a23b093fff28d468f00ae99
Mateli-codes <mateliboniface@gmail.com>
2023-02-16 14:10:13 -0800

    task 6 fix commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,10 +13,10 @@ int b;
 long int c;
 long long int d;
 float f;
-printf("size of char: %lu byte(s)\n", sizeof(a));
-printf("size of int: %lu byte(s)\n", sizeof(b));
-printf("size of long int: %lu byte(s)\n", sizeof(c));
-printf("size of long long int: %lu byte(s)\n", sizeof(d));
-printf("size of float: %lu byte(s)\n", sizeof(f));
+printf("size of char: %zu byte(s)\n", sizeof(a));
+printf("size of int: %zu byte(s)\n", sizeof(b));
+printf("size of long int: %zu byte(s)\n", sizeof(c));
+printf("size of long long int: %zu byte(s)\n", sizeof(d));
+printf("size of float: %zu byte(s)\n", sizeof(f));
 return (0);
 }
