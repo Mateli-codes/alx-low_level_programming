cf46b83c6f836f20ce37e66e0a4c9c908104cb14
Mateli-codes <mateliboniface@gmail.com>
2023-02-16 21:31:23 -0800

    task 6 hot fix

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,10 +8,10 @@
 
 int main(void)
 {
-printf("size of char: %ZU byte(s)\n", sizeof(char));
-printf("size of int: %ZU byte(s)\n", sizeof(int));
-printf("size of long int: %ZU byte(s)\n", sizeof(long int));
-printf("size of long long int: %ZU byte(s)\n", sizeof(long long int));
-printf("size of float: %ZU byte(s)\n", sizeof(float));
+printf("size of char: %lu byte(s)\n", sizeof(char));
+printf("size of int: %lu byte(s)\n", sizeof(int));
+printf("size of long int: %lu byte(s)\n", sizeof(long int));
+printf("size of long long int: %lu byte(s)\n", sizeof(long long int));
+printf("size of float: %lu byte(s)\n", sizeof(float));
 return (0);
 }
