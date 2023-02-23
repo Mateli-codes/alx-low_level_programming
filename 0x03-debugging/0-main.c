abbb12f79a8ce4a82e52f62045224094201c032d
Mateli-codes <mateliboniface@gmail.com>
2023-02-23 04:08:15 -0800

    task 0

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * positive_or_negative - Test function for positive or negative
+ * Return: 0
+ */
+void positive_or_negative(int i)
+int main(void)
+{
+int i;
+i = 0;
+positive_or_negative(i);
+return (0);
+}
