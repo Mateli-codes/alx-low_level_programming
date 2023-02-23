eea1905539d5f6907e9c96f098e6638351a1607c
Mateli-codes <mateliboniface@gmail.com>
2023-02-23 05:10:21 -0800

    task 1

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -13,10 +13,10 @@ int main(void)
 
         i = 0;
 
-        while (i < 10)
-        {
-                putchar(i); // There is no increment in the while loop
-        }
+        /*while (i < 10)
+        *{
+        *       putchar(i);
+        }*/
 
         printf("Infinite loop avoided! \\o/\n");
 
